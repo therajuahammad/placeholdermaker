d2fd467ae935c830b02b9b9e5aa7e7402f418d38
Raju Ahammad <raju@hastechit.com>
2020-10-29 16:52:50 +0600

    Added Necessary Software

--- use.py
+++ use.py
@@ -6,17 +6,9 @@ i.log_level = 2 #set log level to 2, so we can see errors
 
 # set directories list, where all images will be replaced with placeholders
 dirs = [
-	'C:/Users/Lenovo/Desktop/PlaceHolderMaker/phd'
+	'D:/Others Project/placeholdermaker/phd'
 ]
 
-#i.emulate_placehold_it() #emulate placehold.it service
 i.emulate_csshopper_placeholder() #emulate csshopper placeholder service
-#i.emulate_ubuntu() #emulate Ubuntu style
-#i.emulate_ubuntu_orange() #emulate Ubuntu Orange style
-#i.emulate_envato() #emulate Envato style
-#i.emulate_facebook() #emulate Facebook style
-#i.emulate_twitter() #emulate Twitter style
-#i.emulate_default() #drop to default styling settings
-
 i.walk_recursive(dirs) #recursively find all images in given directories
 i.start() #begin conversion process
